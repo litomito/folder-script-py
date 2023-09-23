3545b4d36a1412330448be60f836746769e51f1d
Linus Toma <linustoma11@hotmail.com>
2023-09-23 02:14:54 +0200

    html, css files update
    The script will now ask you if you want to make html files in html folder and the same with css. It will ask you how many files you want and what to name them.

--- script.py
+++ script.py
@@ -13,8 +13,8 @@ title = input("What would you like the tittle to be? ")
 os.chdir(headname)
 i = open("index.html", "x")
 
-file = open("index.html","w")
-file.write(f'<!DOCTYPE html> \n<html lang="en"> \n<head> \n    <meta charset="UTF-8"> \n    <meta name="viewport" content="width=device-width, initial-scale=1.0"> \n    <title>{title}</title> \n    <link rel="stylesheet" href="./css/style.css"> \n</head> \n<body> \n \n    <script src="./js/script.js"></script> \n</body> \n</html>')
+with open("index.html", "w") as file:
+    file.write(f'<!DOCTYPE html> \n<html lang="en"> \n<head> \n    <meta charset="UTF-8"> \n    <meta name="viewport" content="width=device-width, initial-scale=1.0"> \n    <title>{title}</title> \n    <link rel="stylesheet" href="./css/style.css"> \n</head> \n<body> \n \n    <script src="./js/script.js"></script> \n</body> \n</html>')
 file.close()
 
 
@@ -28,3 +28,30 @@ s = open("style.css", "x")
 
 os.chdir("../js")
 j = open("script.js", "x")
+
+
+Q_html = input('Do you want more HTML files in html folder? (y/n): ')
+os.chdir("../html")
+
+if Q_html == "y":
+    num = int(input('How many HTML files would you like? '))
+
+    for i in range(num):
+        file_name = input('Enter the name for the HTMl file: ')
+        with open(f'{file_name}.html', 'w') as html_file:
+            html_file.write(f'<!DOCTYPE html> \n<html lang="en"> \n<head> \n    <meta charset="UTF-8"> \n    <meta name="viewport" content="width=device-width, initial-scale=1.0"> \n    <title>{file_name}</title> \n    <link rel="stylesheet" href="./css/style.css"> \n</head> \n<body> \n \n    <script src="./js/script.js"></script> \n</body> \n</html>')
+else:
+    print('')
+
+Q_css = input('Would you like more CSS files in your css folder (y/n): ')
+os.chdir("../css")
+
+if Q_css == "y":
+    num2 = int(input('How many CSS files would you like? '))
+
+    for i in range(num2):
+        file_name_css = input('Enter the name for the CSS file: ')
+        with open(f'{file_name_css}.css', 'w') as css_file:
+            pass
+else:
+    print('Goodbye')
